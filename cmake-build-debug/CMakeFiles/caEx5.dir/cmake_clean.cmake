40e24579758d69ebecc13965216a75e8c1c682c3
Eddie Gutman <edigutman@gmail.com>
2021-01-14 23:06:34 +0200

    Added project files

---
+++ cmake-build-debug/CMakeFiles/caEx5.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/caEx5.dir/main.c.o"
+  "caEx5"
+  "caEx5.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/caEx5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/caEx5.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for caEx5.
+# This may be replaced when dependencies are built.
